5cba0d16685c5d50feeafce787ea4ba1df7cc4cf
Liam Ramalingam <liamram28@gmail.com>
2023-08-01 13:11:35 +0200

    comments have been added to this code

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,4 @@
-//let userInput = prompt("Please enter a word:");
+// prompts the user to enter a word 
+let userInput = prompt("Please enter a word:");
+// displays the word
 console.log("hello world");
